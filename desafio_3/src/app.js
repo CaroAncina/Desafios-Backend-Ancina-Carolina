a91128ef3a9aef77a06178bcd03eb8652442250c
Caro Ancina <caroancinacabj@gmail.com>
2024-04-15 22:38:50 -0300

    se agregan productos al json, y los endpoints solicitados

--- desafio_3/src/app.js
+++ desafio_3/src/app.js
@@ -1,25 +1,44 @@
-const ProductManager = require('./ProductManager.js')
-const express = require('express')
-const manager = new ProductManager('./src/Productos.json')
+const express = require('express');
+const ProductManager = require('./ProductManager.js');
+
+const manager = new ProductManager('./Productos.json');
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.urlencoded({ extended: true }));
+
+//1er. endpoint 
+app.get('/products', async (req, res) => {
+    try {
+        const limit = req.query.limit ? parseInt(req.query.limit) : undefined;
+        const products = await manager.getproducts();
+        const response = limit ? products.slice(0, limit) : products;
+
+        res.json(response);
+    } catch (error) {
+        console.log(error)
+        return res.send('Error al cargar productos')
+    }
+});
 
 
-const app = express()
+//2do. endpoint
+app.get('/products/:pid', async (req, res) => {
+    try {
+        const productId = parseInt(req.params.pid);
+        const product = await manager.getproductsbyId(productId);
 
-const PORT = 8080
-app.use(express.urlencoded({ extended: true }))
-
-
-//req.params
-
-
-
-
-
-
-//req.querys
-
-
-
+        if (product) {
+            res.json(product);
+        } else {
+            res.status(404).json({ error: 'Producto no encontrado' });
+        }
+    } catch (error) {
+        console.log(error);
+        res.send('Error al buscar el producto');
+    }
+});
 
 
 app.listen(PORT, () => {
